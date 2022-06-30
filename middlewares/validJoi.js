a8f65d3d2dcde968356531da4e8aea0130b12ff7
Igormcf <igormaciel82@gmail.com>
2022-06-30 01:09:25 -0300

    Corrige erro no requisito 03

--- middlewares/validJoi.js
+++ middlewares/validJoi.js
@@ -6,9 +6,9 @@ const characterDTO = Joi.object({
 
 const validMiddleWare = (req, res, next) => {
   const { error } = characterDTO.validate(req.body);
-  const [message] = error.details.map((e) => e.message);
-
+  
   if (error) {
+    const [message] = error.details.map((e) => e.message);
     return error.details[0].message.includes('required') ? res.status(400).json({ message })
       : res.status(422).json({ message });
   }
